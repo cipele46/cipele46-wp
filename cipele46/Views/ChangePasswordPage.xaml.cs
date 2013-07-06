18bbfc46481c4a1f46c23c6797ee0d9effa548e3
Stipe Grbić <stipe.grbic@fiveminutes.eu>
2013-07-06 11:15:11 +0200

    Settings page, change password

---
+++ cipele46/Views/ChangePasswordPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace cipele46.Views
+{
+    public partial class ChangePasswordPage : PhoneApplicationPage
+    {
+        public ChangePasswordPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangePasswordAppBarButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
