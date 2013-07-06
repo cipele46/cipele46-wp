d4456d50a9e79e35a53799b55441b3caa58ab05f
Stipe Grbić <stipe.grbic@fiveminutes.eu>
2013-07-06 10:28:42 +0200

    Search page

--- cipele46/Views/MainPage.xaml.cs
+++ cipele46/Views/MainPage.xaml.cs
@@ -10,11 +10,14 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using Coding4Fun.Toolkit.Controls;
 
 namespace cipele46
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        private bool backKeyPressed = false;
+
         // Constructor
         public MainPage()
         {
@@ -41,7 +44,26 @@ namespace cipele46
 
         private void SearchAppBarButton_Click(object sender, EventArgs e)
         {
+            InputPrompt inputPrompt = new InputPrompt
+            {
+                Title = "traži",
+                Message = ""
+            };
 
+            inputPrompt.Completed += new EventHandler<PopUpEventArgs<string, PopUpResult>>(inputPrompt_Completed);
+            backKeyPressed = false;
+            inputPrompt.Show();
+        }
+
+        void inputPrompt_Completed(object sender, PopUpEventArgs<string, PopUpResult> e)
+        {
+            if (backKeyPressed || String.IsNullOrWhiteSpace(((InputPrompt)sender).Value))
+            {
+                return;
+            }
+            String searchKeyword = ((InputPrompt)sender).Value;
+            NavigationContext.QueryString.Add("searchKeyword", searchKeyword);
+            NavigationService.Navigate(new Uri("/Views/SearchPage.xaml"));
         }
 
         private void MyAdsAppBarButton_Click(object sender, EventArgs e)
