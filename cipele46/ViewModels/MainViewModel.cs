07becec834cec28746a4df02c7a836f4b14bdeb7
Toni Petrina <toni.petrina@gmail.com>
2013-07-06 10:43:27 +0200

    Fixing category logic

--- cipele46/ViewModels/MainViewModel.cs
+++ cipele46/ViewModels/MainViewModel.cs
@@ -1,13 +1,35 @@
-﻿using System.Net;
+﻿using cipele46.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace cipele46.ViewModels
 {
     public class MainViewModel : ViewModelBaseEx
     {
+        private ObservableCollection<category> _categories;
+
         public bool IsDataLoading { get; set; }
         public bool IsDataLoaded { get; set; }
 
-        internal async void LoadDataAsync()
+        public ObservableCollection<category> Categories
+        {
+            get { return _categories; }
+            set
+            {
+                _categories = value;
+                Set(ref _categories, value);
+            }
+        }
+
+        public MainViewModel()
+        {
+            _categories = new ObservableCollection<category>();
+        }
+
+        internal async Task LoadDataAsync()
         {
             if (IsDataLoading)
                 return;
@@ -15,7 +37,8 @@ namespace cipele46.ViewModels
 
             var client = new WebClient();
             var data = await client.DownloadStringTaskAsync("http://dev.fiveminutes.eu/cipele/api/categories");
-            var categories = await JsonConvertEx.DeserializeObjectAsync<Model.category[]>(data);
+            foreach (var category in await JsonConvertEx.DeserializeObjectAsync<Model.category[]>(data))
+                Categories.Add(category);
 
             IsDataLoading = false;
             IsDataLoaded = true;
