6345532078b4be0de39874c27cb001b849832ac5
Stipe Grbić <stipe.grbic@fiveminutes.eu>
2013-07-06 17:27:25 +0200

    Login to send message

--- cipele46/Views/DetailsPage.xaml.cs
+++ cipele46/Views/DetailsPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Phone.Controls;
 using Microsoft.Phone.Tasks;
 using System;
+using cipele46.Model;
+using System.Windows;
 
 namespace cipele46.Views
 {
@@ -21,7 +23,19 @@ namespace cipele46.Views
 
         private void MessageAppBarButton_Click(object sender, System.EventArgs e)
         {
-            NavigationService.Navigate(new Uri("/Views/SendMessagePage.xaml", UriKind.Relative));
+            user user = ((App)Application.Current).User;
+            if (user == null)
+            {
+                MessageBoxResult result = MessageBox.Show("Za slanje poruke potrebna je prijava", "Prijavite se", MessageBoxButton.OKCancel);
+                if (result.Equals(MessageBoxResult.OK))
+                {
+                    NavigationService.Navigate(new Uri("/Views/LoginPage.xaml", UriKind.Relative));
+                }               
+            }
+            else
+            {
+                NavigationService.Navigate(new Uri("/Views/SendMessagePage.xaml", UriKind.Relative));
+            }
         }
 
         private void FavoriteAppBarButton_Click(object sender, System.EventArgs e)
