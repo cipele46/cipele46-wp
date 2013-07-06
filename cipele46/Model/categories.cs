7e148e4c2a2df5c42cff4d1a5fc9812233033833
Toni Petrina <toni.petrina@gmail.com>
2013-07-06 10:24:12 +0200

    Serialization is working!

--- cipele46/Model/categories.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace cipele46.Model
-{
-    public class categories
-    {
-        public List<category> all { get; set; }
-    }
-}
--- cipele46/ViewModels/MainViewModel.cs
+++ cipele46/ViewModels/MainViewModel.cs
@@ -4,13 +4,21 @@ namespace cipele46.ViewModels
 {
     public class MainViewModel : ViewModelBaseEx
     {
+        public bool IsDataLoading { get; set; }
         public bool IsDataLoaded { get; set; }
 
         internal async void LoadDataAsync()
         {
+            if (IsDataLoading)
+                return;
+            IsDataLoading = true;
+
             var client = new WebClient();
             var data = await client.DownloadStringTaskAsync("http://dev.fiveminutes.eu/cipele/api/categories");
-            var categories = await JsonConvertEx.DeserializeObjectAsync<Model.categories>(data);
+            var categories = await JsonConvertEx.DeserializeObjectAsync<Model.category[]>(data);
+
+            IsDataLoading = false;
+            IsDataLoaded = true;
         }
     }
 }
