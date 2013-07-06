06c5dd57ff8f9ae0e069d1cc19f168827b8e1f97
Toni Petrina <toni.petrina@gmail.com>
2013-07-06 17:16:03 +0200

    Updating because of API changes

--- cipele46/Model/ad.cs
+++ cipele46/Model/ad.cs
@@ -14,18 +14,29 @@ namespace cipele46.Model
         closed = 3
     }
 
+    public class ad_mage
+    {
+        public string url { get; set; }
+        public ad_mage large { get; set; }
+        public ad_mage mob_large { get; set; }
+        public ad_mage medium { get; set; }
+        public ad_mage small { get; set; }
+    }
+
     public class ad
     {
-        public int status { get; set; }
+        public int ad_type { get; set; }
+        public int category_id { get; set; }
+        public int city_id { get; set; }
+        public string created_at { get; set; }
         public string description { get; set; }
-        public int districtID { get; set; }
-        public string phone { get; set; }
-        public int categoryID { get; set; }
-        public string title { get; set; }
-        public string imageUrl { get; set; }
-        public int id { get; set; }
-        public int cityID { get; set; }
-        public int type { get; set; }
         public string email { get; set; }
+        public int id { get; set; }
+        public ad_mage image { get; set; }
+        public string phone { get; set; }
+        public int status { get; set; }
+        public string title { get; set; }
+        public string updated_at { get; set; }
+        public int? user_id { get; set; }
     }
 }
