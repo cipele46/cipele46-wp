d1884a80c13f920a8f1dfaafb838d64dd4dd2f84
Stipe Grbić <stipe.grbic@fiveminutes.eu>
2013-07-13 20:53:01 +0200

    search

---
+++ cipele46/ViewModels/SearchViewModel.cs
@@ -0,0 +1,73 @@
+﻿using cipele46.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cipele46.ViewModels
+{
+    public class SearchViewModel : ViewModelBaseEx
+    {
+        private bool _isDataLoading;
+        private bool _isDataLoaded;
+        private String _searchKeyword;
+
+        private ObservableCollection<AdViewModel> _searchAds;
+
+        public SearchViewModel()
+        {
+            _searchAds = new ObservableCollection<AdViewModel>();
+        }
+
+        public bool IsDataLoading
+        {
+            get { return _isDataLoading; }
+            set { Set(ref _isDataLoading, value); }
+        }
+        public bool IsDataLoaded
+        {
+            get { return _isDataLoaded; }
+            set { Set(ref _isDataLoaded, value); }
+        }
+
+        public ObservableCollection<AdViewModel> SearchAds
+        {
+            get { return _searchAds; }
+            set { Set(ref _searchAds, value); }
+        }
+
+        public String SearchKeyword
+        {
+            get { return _searchKeyword; }
+            set { Set(ref _searchKeyword, value); }
+        }
+
+        internal async Task SearchDataAsync(String keyword)
+        {
+            if (IsDataLoading)
+                return;
+            IsDataLoading = true;
+            IsDataLoaded = false;
+
+            SearchKeyword = keyword;
+            SearchAds.Clear();
+
+            // ensure that categories and counties are loaded along with ads
+            var taskAds = new WebClient().DownloadStringTaskAsync(Endpoints.AdsUrl + "?query=" + keyword);
+            await TaskEx.WhenAll(taskAds); 
+
+            // .Result is fine also
+            var data = await taskAds;
+            foreach (var ad in await JsonConvertEx.DeserializeObjectAsync<ad[]>(data))
+            {
+                SearchAds.Add(new AdViewModel(ad));
+            }
+
+            IsDataLoading = false;
+            IsDataLoaded = true;
+        }
+    }
+}
--- cipele46/Views/SearchPage.xaml.cs
+++ cipele46/Views/SearchPage.xaml.cs
@@ -7,28 +7,62 @@ using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
+using cipele46.ViewModels;
+using Coding4Fun.Toolkit.Controls;
 
 namespace cipele46.Views
 {
     public partial class SearchPage : PhoneApplicationPage
     {
+        SearchViewModel _viewModel;
+
         public SearchPage()
         {
             InitializeComponent();
-
-            this.Loaded += new RoutedEventHandler(PhoneApplicationPage_Loaded);
+            this.DataContext = _viewModel = new SearchViewModel();
+            this.Loaded += new RoutedEventHandler(PhoneApplicationPage_Loaded);            
         }
 
         private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
         {
             String searchKeyword;
             NavigationContext.QueryString.TryGetValue("searchKeyword", out searchKeyword);
-            MessageBox.Show(searchKeyword);
+
+            _viewModel.SearchDataAsync(searchKeyword);
+
+            //MessageBox.Show(searchKeyword);
         }
 
         private void SearchAppBarButton_Click(object sender, EventArgs e)
         {
+            InputPrompt inputPrompt = new InputPrompt
+            {
+                Title = "traži",
+                Message = ""
+            };
 
+            inputPrompt.Completed += new EventHandler<PopUpEventArgs<string, PopUpResult>>(inputPrompt_Completed);
+            inputPrompt.Show();
+        }
+
+        void inputPrompt_Completed(object sender, PopUpEventArgs<string, PopUpResult> e)
+        {
+            if (String.IsNullOrWhiteSpace(((InputPrompt)sender).Value))
+            {
+                return;
+            }
+            String searchKeyword = ((InputPrompt)sender).Value;
+            _viewModel.SearchDataAsync(searchKeyword);
+        }
+
+        private void lbAll_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var item = (sender as ListBox).SelectedItem as AdViewModel;
+            if (item == null)
+                return;
+
+            App.SelectedAd = item;
+            NavigationService.Navigate(new Uri("/Views/DetailsPage.xaml", UriKind.Relative));
         }
     }
 }
